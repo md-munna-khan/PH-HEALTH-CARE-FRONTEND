6df80f35eafa4707c36be69956f5508d0622e52a
md.munna <mk8761174@gmail.com>
2025-11-01 23:08:43 +0600

    66-3 Login component and functionality part-1

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import Login from "@/components/modules/auth/login";
+
+
+const LoginPage = () => {
+    return (
+        <div>
+            <Login/>
+        </div>
+    );
+};
+
+export default LoginPage;
